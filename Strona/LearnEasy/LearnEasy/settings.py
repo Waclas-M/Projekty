09cc1217b6929e22edaf6f909b9b09f6de9cc155
Marcin <weclasmarcin@gmail.com>
2024-02-27 21:15:02 +0100

    New site + first app

--- Strona/LearnEasy/LearnEasy/settings.py
+++ Strona/LearnEasy/LearnEasy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'HomeSite.apps.HomesiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
