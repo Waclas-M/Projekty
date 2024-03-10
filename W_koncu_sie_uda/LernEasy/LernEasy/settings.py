cfe5697449081bc199b1be00a3a4188cb5875887
Marcin <weclasmarcin@gmail.com>
2024-03-10 10:51:42 +0100

    a

--- W_koncu_sie_uda/LernEasy/LernEasy/settings.py
+++ W_koncu_sie_uda/LernEasy/LernEasy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WelcomeSite.apps.WelcomesiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    ('static', 'W_koncu_sie_uda/LernEasy/WelcomeSite/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
