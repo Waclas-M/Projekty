0bbfb1e193f4ad08e3241b17363bcfc493589604
Marcin <weclasmarcin@gmail.com>
2024-03-24 19:10:45 +0100

    app template login and logout layout change

--- W_koncu_sie_uda/LernEasy/WelcomeSite/static/js/script.js
+++ W_koncu_sie_uda/LernEasy/WelcomeSite/static/js/script.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
-
+const header = document.querySelector('header')
 hamburger.addEventListener("click", () => {
 
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    header.classList.toggle("active")
 
 })
 
@@ -12,5 +13,6 @@ document.querySelectorAll(".nav-link").forEach( n => n.
     addEventListener('click',()=>{
         hamburger.classList.remove("active")
         navMenu.classList.remove("active");
+        header.classList.remove("active")
     }))
 
