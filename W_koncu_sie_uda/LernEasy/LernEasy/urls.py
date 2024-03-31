fe0916a206b073e9c44da43599ac55fbcf73d3f0
Marcin <weclasmarcin@gmail.com>
2024-03-31 14:26:13 +0200

    Profile app

--- W_koncu_sie_uda/LernEasy/LernEasy/urls.py
+++ W_koncu_sie_uda/LernEasy/LernEasy/urls.py
@@ -19,12 +19,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path , include
 from users import views as user_views
 from fiszki import views as fiszki_views
+from Profile import views as Profile_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('WelcomeSite.urls')),
-    path('reg/', user_views.register, name='register'),
-    path('app/', fiszki_views.fiszkiapp, name='app_fiszki'),
+    path('rejestracja/', user_views.register, name='register'),
+    path('Zacznij_Się_Uczyć/', fiszki_views.fiszkiapp, name='app_fiszki'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/',  auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/',  auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('Profile', Profile_views.profile_tem, name="Profile")
 ]
--- W_koncu_sie_uda/LernEasy/Profile/views.py
+++ W_koncu_sie_uda/LernEasy/Profile/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile_tem(request):
+    return render(request,template_name="Profile/Profile.html")
